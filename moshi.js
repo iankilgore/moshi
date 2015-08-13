48fa8e8d74c8d5291b63d100f29d0c002e35e55d
iankilgore <ianmacduffkilgore@gmail.com>
2015-08-13 12:48:08 -0600

    night light function rewrite

--- moshi.js
+++ moshi.js
@@ -150,6 +150,19 @@ function Moshi()
   }
   this.stopNightLight=function() {
     						// Same with off.
+                var slide_array = [
+              	"<style>body {background-color: #1a0aff;}</style>",
+              	"<style>body {background-color: #00ffff;}</style>",
+              	"<style>body {background-color: #00ff55;}</style>",
+              	"<style>body {background-color: #a8ff64;}</style>",
+              	"<style>body {background-color: #de0614;}</style>",
+              	"<style>body {background-color: #a806de;}</style>",
+
+              ];
+              slide_counter++;
+            	if(slide_counter > (slide_array.length - 1)){
+            		slide_counter = 0;
+            	}
   }
   this.getTemperature=function() {
     						// Get temperature will get the temperature from the widget.
